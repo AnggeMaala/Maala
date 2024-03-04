8e823c152c9f81a15faacbb79086ef448836c8cf
AnggeMaala <135560663+AnggeMaala@users.noreply.github.com>
2024-03-04 20:55:19 +0800

    Add files via upload

---
+++ Maala-Prelim/Maala-Prelim/Maala/app/Views/welcome_message.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pizza Palace</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Pup Store</h1>
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#menu">Menu</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section id="home">
+        <div class="container">
+            <h2>Welcome to Pup Store</h2>
+            <p>We sell the most authentic dog products!</p>
+        </div>
+    </section>
+
+    <section id="menu">
+        <div class="container">
+            <h2>Our Products</h2>
+            <div class="menu-item">
+                <h3>Pup Foamy Shampoo</h3>
+                <p>Buddy Wash Original Lavender & Mint Dog Shampoo & Conditioner.</p>
+            </div>
+            <div class="menu-item">
+                <h3>Pup Foamy Shampoo</h3>
+                <p>Formulated with Oatmeal and Keratin protein that keeps skin and fur healthy, smooth and soft to touch.</p>
+            </div>
+            <div class="menu-item">
+                <h3>Pup Perfume</h3>
+                <p>It is absolutely safe to spray your dog with a dog perfume or dog cologne if its formulated with safety as the number one priority.</p>
+            </div>
+        </div>
+    </section>
+
+    <section id="contact">
+        <div class="container">
+            <h2>Contact Us</h2>
+            <p>For orders and inquiries, please call: 123-456-7890</p>
+        </div>
+    </section>
+
+  
+</body>
+</html>
